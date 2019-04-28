23dbe5b69e9c073e69dab2c5fea5934c183c63b5
giancampo <campo.gian@gmail.com>
2019-04-28 15:20:16 +0200

    Create scrapeNavigationLinks.py

---
+++ scrapeNavigationLinks.py
@@ -0,0 +1,16 @@
+from lxml import html
+import requests
+
+urls = open("urls.txt", "r")
+results_file = open("results.txt", "a+")
+
+for item in urls:
+    url = item.rstrip("\n")
+    page = requests.get(url)
+    tree = html.fromstring(page.content)
+    text = tree.xpath('//ul[@id="menu-primary-items"]/li/a/text()')
+    results_file.write("%s,%s\n" % (url, text))
+    print ("SCRAPING " + url)
+    print (text, "\n")
+
+results_file.close()
