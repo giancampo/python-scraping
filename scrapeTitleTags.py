e1c3f43d9f36c493dbea1a3d150c13d3cbb9e19f
giancampo <campo.gian@gmail.com>
2019-04-28 13:00:07 +0200

    Create scrapeTitleTags.py

---
+++ scrapeTitleTags.py
@@ -0,0 +1,16 @@
+from lxml import html
+import requests
+
+urls = open("urls.txt", "r")
+results_file = open("results.txt", "a+")
+
+for item in urls:
+    url = item.rstrip("\n")
+    page = requests.get(url)
+    tree = html.fromstring(page.content)
+    text = tree.xpath('//title/text()')
+    results_file.write("%s,%s\n" % (url, text))
+    print ("SCRAPING " + url)
+    print (text, "\n")
+
+results_file.close()
